525dae4312a5a0e793f34807531be9439d6db20f
Zdeněk Jelínek <zdenek.jelinek@hotmail.com>
2018-12-29 11:20:51 +0100

    Introduced data collection into setup and integrated into the basic view

--- src/Rcm.Web/Pages/Index.cshtml.cs
+++ src/Rcm.Web/Pages/Index.cshtml.cs
@@ -1,32 +1,40 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Rcm.Measurement.Api;
+using Rcm.DataCollection.Api;
 
 namespace Rcm.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly IMeasurementProvider _measurementProvider;
+        private readonly ICollectedDataAccessor _collectedDataAccessor;
 
         public DateTimeOffset MeasurementTime { get; private set; }
         public decimal Temperature { get; private set; }
         public decimal Pressure { get; private set; }
         public decimal Humidity { get; private set; }
-
-        public IndexModel(IMeasurementProvider measurementProvider)
+                
+        public IndexModel(ICollectedDataAccessor collectedDataAccessor)
         {
-            _measurementProvider = measurementProvider;
+
+            _collectedDataAccessor = collectedDataAccessor;
         }
-
-        public async Task OnGetAsync()
+        
+        public void OnGet()
         {
-            var measurement = await _measurementProvider.MeasureAsync();
+            var latestMeasurement = _collectedDataAccessor
+                .GetCollectedDataAsync(DateTimeOffset.MinValue, DateTimeOffset.MaxValue)
+                .LastOrDefault();
 
-            MeasurementTime = measurement.Time;
-            Temperature = measurement.CelsiusTemperature;
-            Pressure = measurement.HpaPressure;
-            Humidity = measurement.RelativeHumidity;
+            if (latestMeasurement is null)
+            {
+                return;
+            }
+
+            MeasurementTime = latestMeasurement.Time;
+            Temperature = latestMeasurement.CelsiusTemperature;
+            Pressure = latestMeasurement.HpaPressure;
+            Humidity = latestMeasurement.RelativeHumidity;
         }
     }
 }
