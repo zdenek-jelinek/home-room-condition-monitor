8d0b05fea36473b0481128db2f9c2e47bc7df57c
Zdeněk Jelínek <zdenek.jelinek@hotmail.com>
2018-12-29 10:04:50 +0100

    Introduced CollectedDataRepository implementing ICollectedDataWriter, ICollectedDataAccessor as a trivial in-memory implementation

---
+++ src/Rcm.DataCollection/CollectedDataRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rcm.Common;
+using Rcm.DataCollection.Api;
+
+namespace Rcm.DataCollection
+{
+    public class CollectedDataRepository : ICollectedDataWriter, ICollectedDataAccessor
+    {
+        private readonly ICollection<MeasurementEntry> _entries = new List<MeasurementEntry>();
+
+        public Task StoreAsync(MeasurementEntry value)
+        {
+            _entries.Add(value);
+            return Task.CompletedTask;
+        }
+
+        public IEnumerable<MeasurementEntry> GetCollectedDataAsync(DateTimeOffset start, DateTimeOffset end)
+        {
+            return _entries.Where(e => e.Time >= start && e.Time <= end);
+        }
+    }
+}
