c62a23f2cf87a47339e29265d2c11d4048eb8d29
Zdeněk Jelínek <12820141+zdenek-jelinek@users.noreply.github.com>
2019-01-27 20:34:04 +0100

    Introduced EnvironmentDataStoargeLocation which reads storage location path from DATA_PATH environment variable

---
+++ src/Rcm.Web/Configuration/DataCollection/EnvironmentDataStorageLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rcm.DataCollection.Files;
+
+namespace Rcm.Web.Configuration.DataCollection
+{
+    internal class EnvironmentDataStorageLocation : IDataStorageLocation
+    {
+        public string Path { get; }
+
+        public EnvironmentDataStorageLocation()
+        {
+            Path = Environment.GetEnvironmentVariable("DATA_PATH") ?? "";
+        }
+    }
+}
