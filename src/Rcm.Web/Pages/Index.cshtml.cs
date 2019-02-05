2f356eedd71c76ec2c07dcad8ba8e1d4beba5f19
Zdeněk Jelínek <12820141+zdenek-jelinek@users.noreply.github.com>
2019-02-05 20:14:22 +0100

    Cleaned up IndexModel from unused data

--- src/Rcm.Web/Pages/Index.cshtml.cs
+++ src/Rcm.Web/Pages/Index.cshtml.cs
@@ -1,30 +1,8 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Rcm.Common;
-using Rcm.DataCollection.Api;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Rcm.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ICollectedDataAccessor _collectedDataAccessor;
-
-        // TODO: Make nullable
-        public MeasurementEntry LatestMeasurement { get; private set; }
-                
-        public IndexModel(ICollectedDataAccessor collectedDataAccessor)
-        {
-
-            _collectedDataAccessor = collectedDataAccessor;
-        }
-        
-        public void OnGet()
-        {
-            LatestMeasurement = _collectedDataAccessor
-                .GetCollectedDataAsync(DateTimeOffset.Now.AddDays(-7), DateTimeOffset.Now)
-                .OrderBy(m => m.Time)
-                .LastOrDefault();
-        }
     }
 }
