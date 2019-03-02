4e26a56fd6ff0026b9e3e86c2b6075c05874d242
Zdeněk Jelínek <zdenek.jelinek@hotmail.com>
2019-03-02 21:58:35 +0100

    Introduced status page

---
+++ src/Rcm.Web/Presentation/Status/StubStatusPagePresenter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rcm.Web.Presentation.Status
+{
+    public class StubStatusPagePresenter : IStatusPagePresenter
+    {
+        public ConnectivityStatusModel GetStatus()
+        {
+            return new ConnectivityStatusModel(ConnectionStatusModel.Active, new TimeSpan(5, 22, 14, 15, 845));
+        }
+    }
+}
