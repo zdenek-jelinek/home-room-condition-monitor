b3e8e05c0992c035d077ee5064eaa1e0a4680e13
Zdeněk Jelínek <zdenek.jelinek@hotmail.com>
2019-03-02 12:57:43 +0100

    Adjusted bootstrap script to not load any initial module if current page does not define any

---
+++ src/Rcm.Web/Pages/Status.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rcm.Web.Pages
+{
+    public class StatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
