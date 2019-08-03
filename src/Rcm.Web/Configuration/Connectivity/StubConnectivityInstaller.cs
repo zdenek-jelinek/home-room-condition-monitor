10a572b4b26555d2cd1505dcf8b691a9eb1d4f4b
Zdeněk Jelínek <zdenek.jelinek@hotmail.com>
2019-08-03 14:36:17 +0200

    Removed all code traces of MindSphere

---
+++ src/Rcm.Web/Configuration/Connectivity/StubConnectivityInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rcm.Connector.Api.Status;
+
+namespace Rcm.Web.Configuration.Connectivity
+{
+    public class StubConnectivityInstaller : IInstaller
+    {
+        public void Install(IServiceCollection services)
+        {
+            services.AddTransient<IConnectionStatusAccessor, StubConnectionStatusAccessor>();
+        }
+
+        private class StubConnectionStatusAccessor : IConnectionStatusAccessor
+        {
+            public ConnectionStatus GetStatus() => new ConnectionStatus.NotEnabled();
+        }
+    }
+}
