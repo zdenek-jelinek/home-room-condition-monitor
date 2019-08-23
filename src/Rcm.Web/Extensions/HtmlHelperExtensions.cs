09a4f89c9a331e937059458644049b2094b50e32
Zdeněk Jelínek <zdenek.jelinek@hotmail.com>
2019-08-23 19:28:57 +0200

    Renamed HtmlHelperExtensions.GetActiveClass to GetActiveClassIfCurrentPage

--- src/Rcm.Web/Extensions/HtmlHelperExtensions.cs
+++ src/Rcm.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.AspNetCore.Html;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Rcm.Web.Extensions
 {
     public static class HtmlHelperExtensions
     {
-        public static string GetActiveClass(this IHtmlHelper htmlHelper, string page)
+        public static string GetActiveClassIfCurrentPage(this IHtmlHelper htmlHelper, string page)
         {
             if (htmlHelper.ViewContext.RouteData.Values["page"]?.Equals(page) ?? false)
             {
