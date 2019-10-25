81f0379252cc46e331f976df575aaf140d676f77
Zdeněk Jelínek <12820141+zdenek-jelinek@users.noreply.github.com>
2019-10-25 11:57:37 +0200

    Introduced a new Now page as an index page, showing latest measurement data.

--- src/Rcm.Web/wwwroot/js/Measurements/MeasurementsClient.ts
+++ src/Rcm.Web/wwwroot/js/Measurements/MeasurementsClient.ts
@@ -2,13 +2,27 @@
 
 export class MeasurementsClient
 {
-    async getMeasurementsAsync(start:Date, end:Date):Promise<ReadonlyArray<Measurement>>
+    public async getMeasurementsAsync(start:Date, end:Date):Promise<Measurement[]>
     {
         const response = await fetch(`/api/measurements?start=${start.toISOString()}&end=${end.toISOString()}`);
 
         const measurements:ReadonlyArray<MeasurementContract> = await response.json();
         
         return measurements.map(e => new Measurement(new Date(e.time), e.celsiusTemperature, e.hpaPressure, e.relativeHumidity));
+    }
+
+    public async getLatestMeasurementAsync():Promise<Measurement|undefined>
+    {
+        const now = new Date();
+        const fiveMinutesInMilliseconds = 5 * 60 * 1000;
+        const fiveMinutesAgo = new Date(now.getTime() - fiveMinutesInMilliseconds);
+
+        const measurements:Measurement[] = await this.getMeasurementsAsync(fiveMinutesAgo, now);
+
+        // sort by time descending
+        measurements.sort((a, b) => b.time.getTime() - a.time.getTime());
+
+        return measurements[0];
     }
 }
 
---
+++ src/Rcm.Web/wwwroot/js/Views/Common/View.ts
@@ -0,0 +1,13 @@
+﻿export class View
+{
+    protected setElementText(parent:ParentNode, selector:string, text:string):void
+    {
+        const element = parent.querySelector(selector);
+        if (!element)
+        {
+            throw new Error("Could not set value: No element with selector " + selector + " found");
+        }
+
+        element.textContent = text;
+    }
+}
