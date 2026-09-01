26efeffac581e75778949929c27787c0aab70f97
Zdeněk Jelínek <12820141+zdenek-jelinek@users.noreply.github.com>
2026-09-01 14:29:51 +0200

    Slice buffers at call site

--- src/Rcm.I2c/I2cBus.cs
+++ src/Rcm.I2c/I2cBus.cs
@@ -89,7 +89,7 @@ public sealed class I2cBus : IDisposable
 
         if (_logger.IsEnabled(LogLevel.Trace))
         {
-            _logger.LogTrace("Read {ByteCount} bytes from I2C bus\n{ReadData}", read, FormatBuffer(buffer, read));
+            _logger.LogTrace("Read {ByteCount} bytes from I2C bus\n{ReadData}", read, FormatBuffer(buffer[..read]));
         }
     }
 
@@ -127,7 +127,7 @@ public sealed class I2cBus : IDisposable
 
         if (_logger.IsEnabled(LogLevel.Trace))
         {
-            _logger.LogTrace("Written {ByteCount} bytes to I2C bus\n{WrittenData}", written, FormatBuffer(data, written));
+            _logger.LogTrace("Written {ByteCount} bytes to I2C bus\n{WrittenData}", written, FormatBuffer(data[..written]));
         }
     }
 
@@ -151,9 +151,9 @@ public sealed class I2cBus : IDisposable
         _logger.LogTrace("Selected I2C device at {DeviceAddress:x}", address);
     }
 
-    private static string FormatBuffer(ReadOnlySpan<byte> buffer, int length)
+    private static string FormatBuffer(ReadOnlySpan<byte> buffer)
     {
-        return Convert.ToHexString(buffer[..length]);
+        return Convert.ToHexString(buffer);
     }
 
     public void Dispose()
