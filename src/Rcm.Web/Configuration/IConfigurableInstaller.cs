1c8f2bd78e4070d45ea31264bb1e70c682c616a9
Zdeněk Jelínek <12820141+zdenek-jelinek@users.noreply.github.com>
2026-09-01 21:25:12 +0200

    Remove custom IInstaller infra

--- src/Rcm.Web/Configuration/IConfigurableInstaller.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Rcm.Web.Configuration;
-
-public interface IConfigurableInstaller
-{
-    void Install(IServiceCollection services, IConfiguration configuration);
-}
--- src/Rcm.Web/Configuration/IInstaller.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace Rcm.Web.Configuration;
-
-public interface IInstaller
-{
-    void Install(IServiceCollection services);
-}
